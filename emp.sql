1e9bdff8dd6f39a51c9c3f2b2377a0e0beab55d8
Devrath7 <108065526+Devrath7@users.noreply.github.com>
2026-02-25 00:07:49 +0530

    Create emp.sql

---
+++ emp.sql
@@ -0,0 +1,24 @@
+select <columns list>
+from <tables>
+Where <>
+group by <columnlist>
+having <group criteria>
+order by <>
+Limit <>
+
+select job_title_id,title from public.job_title;
+
+select *,concat(first_name,last_name) as full_name
+from public.employee
+
+
+select * 
+from public.employee
+select * 
+from public.job_title
+select * 
+from public.project
+
+select * 
+from public.timesheet
+
